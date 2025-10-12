9481b7d1d485268789a5e6b63470bfb7e19799dd
TalkoDA <azzi2710@gmail.com>
2025-10-12 17:40:16 +0200

    made generate data

--- scripts/forecast.py
+++ scripts/forecast.py
@@ -95,7 +95,13 @@ def forecast_xgb_days(df, country, param_grid, days_ahead=10, model_params=None,
     forecast_df = pd.DataFrame(future_preds)
     return forecast_df
 
-
+param_grid = {
+            "max_depth": [3, 5, 7, 9],
+            "eta": [0.01, 0.05, 0.1, 0.2],
+            "subsample": [0.6, 0.8, 1.0],
+            "colsample_bytree": [0.6, 0.8, 1.0],
+            "n_estimators": [500, 1000, 1500]
+        }
 
 # === Вызов функции для прогноза на 10 дней вперед === #
 forecast = forecast_xgb_days(df, "Аргентина", param_grid=param_grid, days_ahead=10, tune_params=True, n_lags=3)
--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,21 +1,17 @@
-# === Генерация тестовых данных на 365 дней === #
-np.random.seed(42)
-days = 365
-base_vals = [200, 200, 220, 230, 200, 230, 240, 210, 200, 200, 220, 230, 200, 230, 240, 210]
+import numpy as np
+import pandas as pd
 
-dates = pd.date_range(start="2025-01-01", periods=days)
-fd_vals = [base_vals[i % len(base_vals)] + np.random.randint(-15, 16) for i in range(days)]
+def generate_dates(days=365, start='2025-01-01', country_name='Spain'):
+    np.random.seed(42)
+    days = days
+    base_vals = [200, 210, 220, 230, 240]
 
-df = pd.DataFrame({
-    "month": dates,
-    "country_name": ["Аргентина"] * days,
-    "fd_cnt": fd_vals
-})
+    dates = pd.date_range(start=start, periods=days)
+    fd_vals = [base_vals[i % len(base_vals)] + np.random.randint(-15, 16) for i in range(days)]
 
-param_grid = {
-            "max_depth": [3, 5, 7, 9],
-            "eta": [0.01, 0.05, 0.1, 0.2],
-            "subsample": [0.6, 0.8, 1.0],
-            "colsample_bytree": [0.6, 0.8, 1.0],
-            "n_estimators": [500, 1000, 1500]
-        }
+    df = pd.DataFrame({
+        "month": dates,
+        "country_name": country_name * days,
+        "fd_cnt": fd_vals
+    })
+    return df
